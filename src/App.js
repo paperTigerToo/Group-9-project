a653f4c7daa53eefa1f7466038cfeb35c8c41ba4
paperTigerToo <zacharykochemail@gmail.com>
2025-12-07 21:23:32 -0500

    new data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 import csvFile from "./data.csv"
+import csvNewFile from "./shopping_behavior_updated.csv"
 import TestChart from "./testchart.js"
 import GroupBar from './groupBar'
+import TestBar from "./testbar.js"
 //import files that you are working on and call them in the return
 // use the csvData and pass it into your files
 class App extends Component {
 
   state = { csvData: [] };
+  state = { csvNewData: [] };
 
   componentDidMount() {
     d3.csv(csvFile).then(data => {
       console.log("Loaded CSV:", data);
       this.setState({ csvData: data });
+    });
+    d3.csv(csvNewFile).then(data => {
+      console.log("Loaded CSV:", data);
+      this.setState({ csvNewData: data });
     });
   }
   render() {
@@ -20,7 +27,7 @@ class App extends Component {
     <>
       <h1>Hello World</h1>
       <div className="chart-container">
-        <TestChart id="testchart" csvData={this.state.csvData} width={600} height={400} />
+        <TestBar id="testchart" csvData={this.state.csvNewData} width={600} height={400} />
         <GroupBar id='groupbar' csvData={this.state.csvData} width={700} height={600}  />
       </div>
     </>
